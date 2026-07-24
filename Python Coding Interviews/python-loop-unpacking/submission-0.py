30c27be72598e92bbe9afff87c9898fb27aa2f4e
Solomon Nsumei <solnsumei@users.noreply.github.com>
2026-07-24 10:51:02 +0100

    Add: python-loop-unpacking - submission-0

---
+++ Python Coding Interviews/python-loop-unpacking/submission-0.py
@@ -0,0 +1,17 @@
+from typing import List, Tuple
+
+
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    highest_scoring_pair = ("",0)
+    for name, score in scores:
+        if score > highest_scoring_pair[1]:
+            highest_scoring_pair = name, score
+    
+    return highest_scoring_pair[0]
+
+
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
